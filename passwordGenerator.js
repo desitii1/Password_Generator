2aed2c3cbe13bc72e0c49fba26404b30932cb938
Desalegn <deribedesalegn@gmail.com>
2025-02-07 04:44:33 +0300

    correction for main mistakes

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,40 +1,40 @@
-function generatepassword(passwordLength,includeLowercase,includeUppercase, includeNumbers, includeSymbols ){
+function generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "`~!@#$%^&*()-_=+{}[]\\|;:'<>,.?/\"\"";
+  
+  let allowedChars = "";
+  let password = "";
 
-    const uppercase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const lowercase="abcdefghiklmnopqrstuvwxyz";
-    const numbers="0123456789";
-    const symbols="`~!@#$%^&*()-_=+{}[]\|;:'<>,.?/\"\"";
-    let allowedChars="";
-    let password="";
-    allowedChars+=includeLowercase? lowercase:"";
-    allowedChars+=includeUppercase? uppercase:"";
-    allowedChars+=includeNumbers? numbers:"";
-    allowedChars+=includeSymbols? symbols:"";
+  allowedChars += includeLowercase ? lowercase : "";
+  allowedChars += includeUppercase ? uppercase : "";
+  allowedChars += includeNumbers ? numbers : "";
+  allowedChars += includeSymbols ? symbols : "";
 
-   if(passwordLength < 0){
-      return "your password length can not be zero";
-   }
-   if(allowedChars.length===0){
-     return "please chose at least one option";
-   }
+  if (passwordLength <= 0) {
+      return "Password length cannot be zero or negative";
+  }
 
-   for( let i=0;i<passwordLength;i++){
-     let index=Math.floor( Math.random()*allowedChars.length) ;
-     password+=allowedChars[index];
+  if (allowedChars.length === 0) {
+      return "Please select at least one option";
+  }
 
-   }
+  for (let i = 0; i < passwordLength; i++) {
+      let index = Math.floor(Math.random() * allowedChars.length);
+      password += allowedChars[index];
+  }
 
-
-    return password;
-
+  return password;
 }
-const passwordLength=6;
-const includeLowercase=false;
-const includeUppercase=true;
-const includeNumbers=true;
-const includeSymbols=false;
 
+document.getElementById("generate-btn").addEventListener("click", function() {
+  const passwordLength = document.getElementById("password-length").value;
+  const includeLowercase = document.getElementById("lowercase").checked;
+  const includeUppercase = document.getElementById("uppercase").checked;
+  const includeNumbers = document.getElementById("numbers").checked;
+  const includeSymbols = document.getElementById("symbols").checked;
 
-
-const password=generatepassword(passwordLength,includeLowercase,includeUppercase, includeNumbers, includeSymbols);
-console.log(`Generated password: ${password}`);
+  const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols);
+  document.getElementById("generated-password").textContent = password;
+});
